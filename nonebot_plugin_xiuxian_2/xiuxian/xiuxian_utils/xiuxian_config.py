beef8560f368c9c5f2eccf5e2fd40c5adf4122b2
PackageInstaller <yjzyl9008@gmail.com>
2024-05-01 11:59:33 +0800

    lite change

--- nonebot_plugin_xiuxian_2/xiuxian/xiuxian_utils/xiuxian_config.py
+++ nonebot_plugin_xiuxian_2/xiuxian/xiuxian_utils/xiuxian_config.py
@@ -140,10 +140,10 @@ class JsonConfig:
     
     def read_data(self):
         """配置数据"""
-        if not self.config_jsonpath.exists():
-            self.create_default_config()  # 创建默认配置
         with open(self.config_jsonpath, 'r', encoding='utf-8') as e:
             data = json.load(e)
+            if "group" not in data:
+                data["group"] = []
             return data
         
     def create_default_config(self):
