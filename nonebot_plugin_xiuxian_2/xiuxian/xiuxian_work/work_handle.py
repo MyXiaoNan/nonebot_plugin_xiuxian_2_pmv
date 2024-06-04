36aa210ba1638c9591702108575e5e4f745020b6
2598142880 <140939964+2598142880@users.noreply.github.com>
2024-06-04 22:49:20 +0800

    Update work_handle.py

--- nonebot_plugin_xiuxian_2/xiuxian/xiuxian_work/work_handle.py
+++ nonebot_plugin_xiuxian_2/xiuxian/xiuxian_work/work_handle.py
@@ -5,6 +5,31 @@ from ..xiuxian_utils.item_json import Items
 
 sql_message = XiuxianDateManage()  # sql类
 
+
+def readf(user_id):
+    user_id = str(user_id)
+
+    FILEPATH = PLAYERSDATA / user_id / "workinfo.json"
+    with open(FILEPATH, "r", encoding="UTF-8") as f:
+        data = f.read()
+    return json.loads(data)
+
+
+def savef(user_id, data):
+    user_id = str(user_id)
+
+    if not os.path.exists(PLAYERSDATA / user_id):
+        logger.opt(colors=True).info("<green>用户目录不存在，创建目录</green>")
+        os.makedirs(PLAYERSDATA / user_id)
+
+    FILEPATH = PLAYERSDATA / user_id / "workinfo.json"
+
+    savemode = "w" if os.path.exists(FILEPATH) else "x"
+    with open(FILEPATH, mode=savemode, encoding="UTF-8") as f:
+        f.write(data)
+        f.close()
+    return True
+
 class workhandle(XiuxianJsonDate):
 
     def do_work(self, key, work_list=None, name=None, level="江湖好手", exp=None, user_id=None):
