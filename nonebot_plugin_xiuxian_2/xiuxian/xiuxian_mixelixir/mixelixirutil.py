94b7e3734ccadae22b91beff5bb1c8e73265cfd3
chenyuyo <53853741+cy1159178778@users.noreply.github.com>
2024-07-08 19:21:57 +0800

    补充一药多用判断

--- nonebot_plugin_xiuxian_2/xiuxian/xiuxian_mixelixir/mixelixirutil.py
+++ nonebot_plugin_xiuxian_2/xiuxian/xiuxian_mixelixir/mixelixirutil.py
@@ -91,10 +91,11 @@ async def get_mix_elixir_msg(yaocai):
                                 if is_mix:  # 有可以合成的
                                     if i + o + p <= Llandudno_info["max_num"]:
                                         # 判断背包里药材是否足够(同个药材多种类型)
-                                        num_dict = Counter([*[v["name"]]*i, *[vv["name"]]*o, *[vvv["name"]]*p])
-                                        if any(num_dict[yao["name"]] > yao["num"] for yao in [v, vv, vvv]):
-                                            p += 1
-                                            continue
+                                        if len({v["name"], vv["name"], vvv["name"]}) != 3:
+                                            num_dict = Counter([*[v["name"]]*i, *[vv["name"]]*o, *[vvv["name"]]*p])
+                                            if any(num_dict[yao["name"]] > yao["num"] for yao in [v, vv, vvv]):
+                                                p += 1
+                                                continue
 
                                         mix_elixir_msg[num] = {}
                                         mix_elixir_msg[num]['id'] = id_
