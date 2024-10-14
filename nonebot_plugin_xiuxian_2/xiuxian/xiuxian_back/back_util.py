130258107bc4c3f19c8dd0809b52d6150ba8fd4a
PackageInstaller <yjzyl9008@gmail.com>
2024-10-14 11:54:51 +0800

    修复批量使用境界突破丹药绕开耐药性限制的问题

--- nonebot_plugin_xiuxian_2/xiuxian/xiuxian_back/back_util.py
+++ nonebot_plugin_xiuxian_2/xiuxian/xiuxian_back/back_util.py
@@ -465,7 +465,9 @@ def check_use_elixir(user_id, goods_id, num):
     goods_rank = goods_info['rank']
     goods_name = goods_info['name']
     back = sql_message.get_item_by_good_id_and_user_id(user_id, goods_id)
-    goods_all_num = back['all_num']
+    goods_all_num = back['all_num'] # 数据库里的使用数量
+    remaining_limit = goods_info['all_num'] - goods_all_num  # 剩余可用数量
+
     if goods_info['buff_type'] == "level_up_rate":  # 增加突破概率的丹药
         if goods_rank < user_rank:  # 最低使用限制
             msg = f"丹药：{goods_name}的最低使用境界为{goods_info['境界']}，道友不满足使用条件"
@@ -482,10 +484,15 @@ def check_use_elixir(user_id, goods_id, num):
         else:
             if goods_all_num >= goods_info['all_num']:
                 msg = f"道友使用的丹药：{goods_name}已经达到丹药的耐药性上限！已经无法使用该丹药了！"    
-            else:  # 检查完毕
+            else:
+                if num > remaining_limit:
+                    num = remaining_limit
+                    msg = f"道友使用的数量超过了耐药性上限呢，仅使用了{num}颗！"
+                else:
+                    msg = f"道友成功使用丹药：{goods_name}{num}颗, 下一次突破的成功概率提高{goods_info['buff'] * num}%!"
+
                 sql_message.update_back_j(user_id, goods_id, num, 1)
                 sql_message.update_levelrate(user_id, user_info['level_up_rate'] + goods_info['buff'] * num)
-                msg = f"道友成功使用丹药：{goods_name}{num}颗,下一次突破的成功概率提高{goods_info['buff'] * num}%!"
 
     elif goods_info['buff_type'] == "hp":  # 回复状态的丹药
         if user_info['root'] == "器师":
